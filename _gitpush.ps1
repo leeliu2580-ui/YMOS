2b73d1ff184bcd7c96c4e9c45241667b0f153245
gunhai562 <guanhai562@qq.com>
2026-04-08 07:36:58 +0800

    chore: remove temp gitpush script

--- _gitpush.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item -Path 'D:\0_workspace\trae_2601\ymos\YMOS\_fetch_v2.ps1' -Force -ErrorAction SilentlyContinue
-Remove-Item -Path 'D:\0_workspace\trae_2601\ymos\YMOS\_check_taco.py' -Force -ErrorAction SilentlyContinue
-Set-Location 'D:\0_workspace\trae_2601\ymos\YMOS'
-git add -A
-git commit -m "fix(Eyes): Trump TACO ceasefire - update Iran war market insight"
-git push
