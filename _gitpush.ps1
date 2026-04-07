c2e07e754908555aa45b4a2b30c9f0db9dfbe35f
gunhai562 <guanhai562@qq.com>
2026-04-08 07:52:17 +0800

    feat(Eyes): 鎶曡祫闆疯揪 2026-04-08 - Trump TACO瑙﹀彂椋庨櫓璧勪骇鏆存定

---
+++ _gitpush.ps1
@@ -0,0 +1,5 @@
+Remove-Item -Path 'D:\0_workspace\trae_2601\ymos\YMOS\_price_scan.ps1' -Force -ErrorAction SilentlyContinue
+Set-Location 'D:\0_workspace\trae_2601\ymos\YMOS'
+git add -A
+git commit -m "feat(Eyes): 投资雷达 2026-04-08 - Trump TACO触发风险资产暴涨"
+git push
