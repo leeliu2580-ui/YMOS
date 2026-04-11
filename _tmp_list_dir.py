1e83015d8aee898834474f9d2602812e76a29251
gunhai562 <guanhai562@qq.com>
2026-04-11 20:48:07 +0800

    workspace backup 2026-04-11

--- _tmp_list_dir.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import os
-folder = r"D:\0_workspace\trae_2601\ymos\YMOS\持仓与关注\持仓\BTC_BTC"
-for f in os.listdir(folder):
-    print(f)
--- _tmp_list_hype.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-folder = r"D:\0_workspace\trae_2601\ymos\YMOS\持仓与关注\持仓\HYPE_HYPE"
-if os.path.exists(folder):
-    for f in os.listdir(folder):
-        print(f)
-else:
-    print("FOLDER NOT FOUND")
